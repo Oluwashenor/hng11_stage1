1c9b17aef5906e0a8103d8a2b10610446a721030
Adesina <adeshiname@gmail.com>
2024-07-01 10:00:50 +0100

    Add project files.

---
+++ hng11_stage1/Program.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+
+
+app.MapGet("/api/hello", ([FromQuery] string visitor_name, HttpContext context) =>
+{
+    var ip = context.Connection.RemoteIpAddress;
+    return Results.Ok(new
+    {
+        client_ip=ip,
+
+        greeting=$"Hello, ${visitor_name}, the temperature is 11 degrees Celsius in ${visitor_name}"
+    });
+})
+.WithName("greetings")
+.WithOpenApi();
+
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
